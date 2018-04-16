f650a450f583573b2b6d45e507ba0dbe35a83617
BarWy <b.wysocki@onet.pl>
2018-04-16 23:29:45 +0200

    some_changes

--- app.js
+++ app.js
@@ -5,8 +5,10 @@
 var playBoard;
 const humPlay = 'O';
 const aI ='X';
+let start = ["first","second","third","fourth","fifth","sixth","seventh","eight","nineth" ]
 
     const gamefield = document.querySelectorAll('.game-field');
+
     startGame();
 
 
@@ -17,7 +19,11 @@ const aI ='X';
 
     for(var i = 0; i<gamefield.length; i++) {
         gamefield[i].addEventListener('click', turnClick, false);//tworze nasłuchiwacza zdarzeń Tworzy sie MouseEvent, wykorzystam do wyciągnięcia z niego id pola
+
+
+
     }
+
 
 
 
@@ -35,63 +41,74 @@ function comp() {
         for(var i =0; i<playBoard.length; i++){
 
              choseField.push(playBoard[i])
+            console.log(choseField)
+
+
         }
+        var clickField = choseField[choseField.length - 1]
+        var clickID = start.indexOf(clickField);
+        var newArray = start.splice(clickID,1);
 
-    switch (true) {
+    //console.log(newArray)
+    //console.log(start)
 
-        case choseField[choseField.length - 1] == 'first':
-            document.getElementById('fifth').innerText = aI;
-            document.getElementById('fifth').removeEventListener('click', turnClick, false)
-            break
 
-        case choseField[choseField.length - 1] == 'second':
+     switch (true) {
+
+        case choseField[choseField.length - 1] == 'first' && choseField[choseField.length - 2] =='second':
             document.getElementById('third').innerText = aI;
-            document.getElementById('third').removeEventListener('click', turnClick, false);
-            break
-
-        case choseField[choseField.length - 1] == 'third':
-            document.getElementById('second').innerText = aI;
-            document.getElementById('second').removeEventListener('click', turnClick, false);
-            break
-
-        case choseField[choseField.length - 1] == 'fourth':
-            document.getElementById('fifth').innerText = aI;
-            document.getElementById('fifth').removeEventListener('click', turnClick, false);
-            break
-
-        case choseField[choseField.length - 1] == 'fifth':
-            document.getElementById('sixth').innerText = aI;
-            document.getElementById('sixth').removeEventListener('click', turnClick, false);
-            break
-
-        case choseField[choseField.length - 1] == 'sixth':
-            document.getElementById('fourth').innerText = aI;
-            document.getElementById('fourth').removeEventListener('click', turnClick, false);
-            break
-        case choseField[choseField.length - 1] == 'seventh':
-            document.getElementById('nineth').innerText = aI;
-            document.getElementById('nineth').removeEventListener('click', turnClick, false);
-            break
-        case choseField[choseField.length - 1] == 'eight':
-            document.getElementById('seventh').innerText = aI;
-            document.getElementById('seventh').removeEventListener('click', turnClick, false);
-            break
-        case choseField[choseField.length - 1] == 'nineth':
-            document.getElementById('eight').innerText = aI;
-            document.getElementById('eight').removeEventListener('click', turnClick, false);
+            document.getElementById('third').removeEventListener('click', turnClick, false)
             break
 
 
-
-
-
-
+    // switch (true) {
+    //
+    //     case choseField[choseField.length - 1] == 'first' || 'second':
+    //         document.getElementById('fifth').innerText = aI;
+    //         document.getElementById('fifth').removeEventListener('click', turnClick, false)
+    //         break
+    //
+    //     case choseField[choseField.length - 1] == 'second':
+    //         document.getElementById('third').innerText = aI;
+    //         document.getElementById('third').removeEventListener('click', turnClick, false);
+    //         break
+    //
+    //     case choseField[choseField.length - 1] == 'third':
+    //         document.getElementById('second').innerText = aI;
+    //         document.getElementById('second').removeEventListener('click', turnClick, false);
+    //         break
+    //
+    //     case choseField[choseField.length - 1] == 'fourth':
+    //         document.getElementById('fifth').innerText = aI;
+    //         document.getElementById('fifth').removeEventListener('click', turnClick, false);
+    //         break
+    //
+    //     case choseField[choseField.length - 1] == 'fifth':
+    //         document.getElementById('sixth').innerText = aI;
+    //         document.getElementById('sixth').removeEventListener('click', turnClick, false);
+    //         break
+    //
+    //     case choseField[choseField.length - 1] == 'sixth':
+    //         document.getElementById('fourth').innerText = aI;
+    //         document.getElementById('fourth').removeEventListener('click', turnClick, false);
+    //         break
+    //     case choseField[choseField.length - 1] == 'seventh':
+    //         document.getElementById('nineth').innerText = aI;
+    //         document.getElementById('nineth').removeEventListener('click', turnClick, false);
+    //         break
+    //     case choseField[choseField.length - 1] == 'eight':
+    //         document.getElementById('seventh').innerText = aI;
+    //         document.getElementById('seventh').removeEventListener('click', turnClick, false);
+    //         break
+    //     case choseField[choseField.length - 1] == 'nineth':
+    //         document.getElementById('eight').innerText = aI;
+    //         document.getElementById('eight').removeEventListener('click', turnClick, false);
+    //         break
+    //
+    }
 
 
     }
 
 
-
-
-}
 
